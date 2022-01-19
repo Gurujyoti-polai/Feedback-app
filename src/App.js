63a0993a829a5015bd99196be0a40e2721c73879
Gurujyoti-polai <79266786+Gurujyoti-polai@users.noreply.github.com>
2022-01-19 14:25:34 +0530

    adding card Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-mport { useState } from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   return (
     <>
       <Header />
-      <div className='container'>
+      <div className="container">
         <FeedbackList feedback={feedback} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
